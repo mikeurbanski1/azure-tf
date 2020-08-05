95c846f86c6f3ff50243785c04262c2a7cd732c3
Mike Urbanski <mike@bridgecrew.io>
2020-08-05 17:51:59 -0500

    add dummy resource

--- main.tf
+++ main.tf
@@ -2,3 +2,12 @@ provider "azurerm" {
   features {}
 }
 
+resource "random_string" "random" {
+  length = 16
+  special = true
+  override_special = "/@£$"
+}
+
+output "out" {
+  value = random_string.random.result
+}

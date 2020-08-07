be1d2084651b154f700335eeb03b3787acd64180
Mike Urbanski <mike@bridgecrew.io>
2020-08-07 09:29:50 -0500

    remove a resource

--- main.tf
+++ main.tf
@@ -8,12 +8,6 @@ resource "random_string" "random" {
   override_special = "/@£$"
 }
 
-resource "random_string" "random3" {
-  length = 16
-  special = true
-  override_special = "/@£$"
-}
-
 output "out" {
   value = random_string.random.result
 }

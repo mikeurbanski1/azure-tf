3898b6969de268c3f43f55e74c09c96716fcd5b0
Mike Urbanski <mike@bridgecrew.io>
2020-08-07 15:26:54 -0500

    update rg ref

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,15 +11,16 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "rg2"
-  location = "Central US"
+data "azurerm_client_config" "current" {}
+
+data "azurerm_resource_group" "tf_rg" {
+  name = "tfstate"
 }
 
 resource "azurerm_storage_account" "storage_account" {
   name                     = "bcazrstorage2"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.tf_rg.name
+  location                 = azurerm_resource_group.tf_rg.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 }

ee78aedd2fe76f4280ca83bb3c0a29d17bfd79a0
Mike Urbanski <mike@bridgecrew.io>
2020-08-07 11:20:55 -0500

    add azure resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,20 +1,33 @@
-# provider "azurerm" {
-#   features {}
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg2"
+  location = "Central US"
+}
+
+resource "azurerm_storage_account" "storage_account" {
+  name                     = "bcazrstorage2"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+
+# resource "random_string" "random" {
+#   length = 16
+#   special = true
+#   override_special = "/@£$"
 # }
 
-resource "random_string" "random" {
-  length = 16
-  special = true
-  override_special = "/@£$"
-}
+# resource "random_string" "random2" {
+#   length = 16
+#   special = true
+#   override_special = "/@£$"
+# }
 
-resource "random_string" "random2" {
-  length = 16
-  special = true
-  override_special = "/@£$"
-}
-
-output "out" {
-  value = random_string.random.result
-}
+# output "out" {
+#   value = random_string.random.result
+# }
 
